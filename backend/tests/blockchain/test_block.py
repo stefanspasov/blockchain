c1c919b39406cb6cb452e035baec8ef6ec33cb4b
Stefan <stefanspasov90@gmail.com>
2019-12-05 22:50:26 +0100

    Add hex_to_binary

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,5 @@
 from backend.blockchain.block import Block, GENESIS_DATA
+from backend.util.hex_to_binary import hex_to_binary
 import time 
 from backend.config import MINE_RATE, SECONDS
 
@@ -10,7 +11,7 @@ def test_mine_block():
     assert isinstance(block, Block)
     assert block.data == data
     assert block.last_hash == last_block.hash
-    assert block.hash[0:block.difficulty] == '0' * block.difficulty
+    assert hex_to_binary(block.hash)[0:block.difficulty] == '0' * block.difficulty
 
 def test_genesis():
     genesis = Block.genesis()
